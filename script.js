2281003db1fc00ee93445a08820c299b99576d1e
Brenno Leite <brennoleitem@gmail.com>
2024-02-15 18:52:40 -0300

    finalizando

---
+++ script.js
@@ -0,0 +1,29 @@
+const hour   = document.getElementById('hours');
+const minute = document.getElementById('minutes');
+const second = document.getElementById('seconds');
+
+const relogio = setInterval(function time() {
+
+    let dateToday = new Date();
+
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hr < 10) {
+        hr = "0" + hr;
+    }
+
+    if (min < 10) {
+        min = "0" + min;
+    }
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+    hour.textContent = hr;
+    minute.textContent = min;
+    second.textContent = sec;
+
+});
